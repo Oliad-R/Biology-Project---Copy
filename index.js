4db5cde52b2a6b10db5aa9be4fb771c8c4b26a56
Oliad Rundassa <runol@utschools.ca>
2022-06-04 22:18:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+$(window).load(function () {
+  $(".trigger_popup").click(function() {
+    $(".line6_data").hide();
+    $(".line5_data").hide();
+    $(".line4_data").hide();
+    $(".line3_data").hide();
+    $(".line2_data").hide();
+    $(".line1_data").show();
+  });
+  $(".trigger_popup2").click(function() {
+    $(".line6_data").hide();
+    $(".line5_data").hide();
+    $(".line4_data").hide();
+    $(".line3_data").hide();
+    $(".line2_data").show();
+    $(".line1_data").hide();
+  });
+  $(".trigger_popup3").click(function() {
+    $(".line6_data").hide();
+    $(".line5_data").hide();
+    $(".line4_data").hide();
+    $(".line3_data").show();
+    $(".line2_data").hide();
+    $(".line1_data").hide();
+  });
+  $(".trigger_popup4").click(function() {
+    $(".line6_data").hide();
+    $(".line5_data").hide();
+    $(".line4_data").show();
+    $(".line3_data").hide();
+    $(".line2_data").hide();
+    $(".line1_data").hide();
+  });
+  $(".trigger_popup5").click(function() {
+    $(".line6_data").hide();
+    $(".line5_data").show();
+    $(".line4_data").hide();
+    $(".line3_data").hide();
+    $(".line2_data").hide();
+    $(".line1_data").hide();
+  });
+  $(".trigger_popup6").click(function() {
+    $(".line6_data").show();
+    $(".line5_data").hide();
+    $(".line4_data").hide();
+    $(".line3_data").hide();
+    $(".line2_data").hide();
+    $(".line1_data").hide();
+  });
+});
